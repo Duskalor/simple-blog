b8955ce80192658d973d101bc75a4ab5e4fb067b
duskalor <duskalor1@gmail.com>
2024-10-07 01:58:46 -0500

    FIST COMMIT

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    autor: z.string(),
+    description: z.string(),
+    date: z.date(),
+    image: z.string(),
+  }),
+});
+
+export const collections = {
+  blog,
+};
